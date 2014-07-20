d753ce3fe401e507dc45bb71cfacc102b149021f
hhumml <hhumml@gmail.com>
2014-07-20 09:08:19 +0200

    Alpha build for 1.3.0.

--- floatz.dev.kit/templates/layout.010.center/styles/floatz-1.2.0/scripts/floatz.skiplink.js
+++
@@ -1,8 +0,0 @@
-/* floatz CSS Framework v1.2.0
-   Copyright (c) 1998-2013 by :humml:design
-   Apache License 2.0 http://www.apache.org/licenses/LICENSE-2.0 */
-
-(function(){"use strict";var self={module:{name:"floatz.skiplink",version:"1.2.0",start:start}};var floatz=window.floatz;var module=self.module;function start(){fixSkipLinks();fixSkipLinkAnchors();floatz.log(floatz.LOGLEVEL.INFO,"Module "+module.name+" started",module.name);}
-function fixSkipLinks(){if(!floatz.browser.isMobile()&&(floatz.browser.webkit||floatz.browser.opera)){floatz.log(floatz.LOGLEVEL.DEBUG,"Adding click handler that sets focus to all flz_skipnav a",module.name);$(".flz_skipnav a").click(function(){$(this.href.substring(this.href.indexOf("#"),this.href.length)).focus();});}}
-function fixSkipLinkAnchors(){if(!floatz.browser.isMobile()&&(floatz.browser.webkit||$.browser.opera||$.browser.msie)){floatz.log(floatz.LOGLEVEL.DEBUG,"Adding tabindex: -1 to all a:flz_anchor",module.name);$("a.flz_anchor").attr("tabindex","-1");}}
-floatz.loadedModules.push(module);floatz.log(floatz.LOGLEVEL.INFO,"Module "+module.name+" loaded",module.name);return self;}());
--- floatz/scripts/floatz.mobile.js
+++ floatz/scripts/floatz.mobile.js
@@ -1,8 +1,8 @@
-/* floatz CSS Framework v1.2.0
+/* floatz CSS Framework v1.3.0
    Copyright (c) 1998-2013 by :humml:design
    Apache License 2.0 http://www.apache.org/licenses/LICENSE-2.0 */
 
-window.floatz.mobile=(function(){"use strict";var self={module:{name:"floatz.mobile",version:"1.2.0",start:start,bind:bind},bind:bind};var floatz=window.floatz;var module=self.module;function start(){if(floatz.browser.isMobileWebkit()){if(document.styleSheets.length>0){floatz.log(floatz.LOGLEVEL.DEBUG,"Adding -webkit-text-size-adjust: none to * in mobile webkit",module.name);document.styleSheets[0].addRule("*","-webkit-text-size-adjust: none;");}else{floatz.log(floatz.LOGLEVEL.ERROR,"Adding mobile webkit specific styles failed. Please load the stylesheets before the script modules.",module.name);}
+window.floatz.mobile=(function(){"use strict";var self={module:{name:"floatz.mobile",version:"1.3.0",start:start,bind:bind},bind:bind};var floatz=window.floatz;var module=self.module;function start(){if(floatz.isMobileWebkit()){if(document.styleSheets.length>0){floatz.log(floatz.LOGLEVEL.DEBUG,"Adding -webkit-text-size-adjust: none to * in mobile webkit",module.name);document.styleSheets[0].addRule("*","-webkit-text-size-adjust: none;");}else{floatz.log(floatz.LOGLEVEL.ERROR,"Adding mobile webkit specific styles failed. Please load the stylesheets before the script modules.",module.name);}
 bind();}
 floatz.log(floatz.LOGLEVEL.INFO,"Module "+module.name+" started",module.name);}
 function bind(){floatz.log(floatz.LOGLEVEL.DEBUG,"Adding -webkit-overflow-scrolling: touch to flz_scrollbar, flz_scrollbox on mobile webkit",module.name);floatz.log(floatz.LOGLEVEL.DEBUG,"Adding overflow:scroll to flz_scrollpanel, flz_scrollbox on mobile webkit",module.name);$(".flz_scrollpanel, .flz_scrollbox").css("-webkit-overflow-scrolling","touch").css("overflow","scroll");}
