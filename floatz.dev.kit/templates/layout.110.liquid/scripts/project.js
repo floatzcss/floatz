bba69bca3087e8ef3b435fce0895970609e53de7
hhumml <hhumml@gmail.com>
2014-07-20 09:35:08 +0200

    Alpha build for 1.3.0.

--- floatz.dev.kit/templates/layout.110.liquid/scripts/project.js
+++ floatz.dev.kit/templates/layout.110.liquid/scripts/project.js
@@ -1,9 +1,9 @@
 // Load jquery, less and floatz in correct order
-$LAB.script("./styles/floatz-1.2.0/scripts/jquery-1.11.1.min.js")
-	.script("./styles/floatz-1.2.0/scripts/ua-parser-0.7.0.min.js").wait()
-	.script("./styles/floatz-1.2.0/scripts/floatz.js").wait()
-	.script("./styles/floatz-1.2.0/scripts/floatz.mobile.js")
-	.script("./styles/floatz-1.2.0/scripts/floatz.skiplink.js")
+$LAB.script("./styles/floatz-1.3.0/scripts/jquery-1.11.1.min.js")
+	.script("./styles/floatz-1.3.0/scripts/ua-parser-0.7.0.min.js").wait()
+	.script("./styles/floatz-1.3.0/scripts/floatz.js").wait()
+	.script("./styles/floatz-1.3.0/scripts/floatz.mobile.js")
+	.script("./styles/floatz-1.3.0/scripts/floatz.skiplink.js")
 	.wait(function() {
 		$(document).ready(function() {
 			
