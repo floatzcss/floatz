3f0ab2299251ffba8cfff4a36b6e0a365a53af7a
hhumml <hhumml@gmail.com>
2015-02-25 08:01:09 +0100

    CR #12

---
+++ floatz.dev.kit/tools/templatebuilder/scripts/demo.js
@@ -0,0 +1,14 @@
+$LAB.script("./styles/floatz-1.3.0/scripts/jquery-1.11.2.min.js")
+	.script("./styles/floatz-1.3.0/scripts/ua-parser-0.7.3.min.js").wait()
+	.script("./styles/floatz-1.3.0/scripts/floatz.js").wait()
+	.script("./styles/floatz-1.3.0/scripts/floatz.skiplink.js")
+	.wait(function() {
+		$(document).ready(function() {
+			// Start floatz modules
+			floatz.start({
+				debug : true,
+				logLevel : floatz.LOGLEVEL.DEBUG,
+				modules : ["floatz.skiplink"]
+			});
+		});
+	});
