ad7fdf26674a9ed4c4a188e259becba1288aa1ba
hhumml <hhumml@gmail.com>
2015-08-15 12:29:26 +0200

    Issue #44

--- floatz.dev.kit/templates/layout.empty/styles/floatz-1.4.0/scripts/floatz.js
+++ floatz.dev.kit/templates/layout.empty/styles/floatz-1.4.0/scripts/floatz.js
@@ -2,7 +2,7 @@
    Copyright (c) 1998-2015 by :hummldesign
    Apache License 2.0 http://www.apache.org/licenses/LICENSE-2.0 */
 
-window.floatz=(function(floatz,$){"use strict";var self={LOGLEVEL:{ERROR:0,WARN:1,INFO:2,DEBUG:3},loadedModules:[],module:{name:"floatz",version:"1.4.0"},start:start,log:log,string:{lpad:lpad,rpad:rpad},userAgent:window.UAParser?new UAParser().getResult():{},isMobileWebkit:isMobileWebkit,isMobile:isMobile};var LOGLEVEL=self.LOGLEVEL;var loadedModules=self.loadedModules;var ua=self.userAgent;var module=self.module;var config={debug:false,logLevel:LOGLEVEL.DEBUG};function start(options){var i,j;updateConfig(options);log(LOGLEVEL.INFO,self.userAgent.ua,module.name);log(LOGLEVEL.INFO,"Module "+module.name+" started",module.name);for(i=0;i<loadedModules.length;i++){var canStart=config.modules===undefined;if(!canStart){for(j=0;j<config.modules.length;j++){if(loadedModules[i].name===config.modules[j]){canStart=true;break;}}}
+window.floatz=(function(floatz,$){"use strict";var self={LOGLEVEL:{ERROR:0,WARN:1,INFO:2,DEBUG:3},loadedModules:[],module:{name:"floatz",version:"1.4.0"},start:start,log:log,string:{lpad:lpad,rpad:rpad},matchMedia:matchMedia,userAgent:window.UAParser?new UAParser().getResult():{},isMobileWebkit:isMobileWebkit,isMobile:isMobile};var LOGLEVEL=self.LOGLEVEL;var loadedModules=self.loadedModules;var ua=self.userAgent;var module=self.module;var config={debug:false,logLevel:LOGLEVEL.DEBUG};function start(options){var i,j;updateConfig(options);log(LOGLEVEL.INFO,self.userAgent.ua,module.name);log(LOGLEVEL.INFO,"Module "+module.name+" started",module.name);for(i=0;i<loadedModules.length;i++){var canStart=config.modules===undefined;if(!canStart){for(j=0;j<config.modules.length;j++){if(loadedModules[i].name===config.modules[j]){canStart=true;break;}}}
 if(canStart){loadedModules[i].start();}}
 if(config.modules!==undefined&&config.modules!==null){for(i=0;i<config.modules.length;i++){var found=false;for(j=0;j<loadedModules.length;j++){if(config.modules[i]===loadedModules[j].name){found=true;}}
 if(!found){log(LOGLEVEL.ERROR,"Module "+config.modules[i]+" is invalid or not loaded",module.name);}}}
@@ -22,5 +22,6 @@ var pstr=str;var max=(len-str.length)/c.length;for(var i=0;i<max;i++){pstr=c+pst
 return pstr;}
 function isMobileWebkit(){return ua.engine.name==="WebKit"&&(ua.os.name==="Android"||ua.os.name==="iOS");}
 function isMobile(){return ua.os.name==="Android"||ua.os.name==="iOS"||ua.os.name==="Windows Phone"||ua.os.name==="BlackBerry";}
+function matchMedia(query,matcher){if(window.matchMedia1){var mq=window.matchMedia(query);var callback=function(mq){if(mq.matches){matcher.onMatch();}else if(matcher.onUnMatch){matcher.onUnMatch();}};mq.addListener(callback);callback(mq);}else{log(LOGLEVEL.WARN,"Media query via Javascript not supported in browser",module.name);}}
 if(!window.UAParser){log(LOGLEVEL.ERROR,"Module "+module.name+" depends on ua-parser.js which is not loaded",module.name);return;}
 log(LOGLEVEL.INFO,"Module "+module.name+" loaded",module.name);return self;}(window.floatz,window.jQuery||null));
